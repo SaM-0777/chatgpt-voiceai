28a0f573f6b41d5e0439cfb31a7ffddf97623b33
Soumyaranjan Sahu <sam.2.3.4.5.ranjan@gmail.com>
2023-01-24 04:50:08 +0530

    response is coming

--- src/screens/ChatRoom/ChatRoom.tsx
+++ src/screens/ChatRoom/ChatRoom.tsx
@@ -1,9 +1,13 @@
 import { useState, useCallback, useEffect } from "react";
-import { View, Text } from "react-native";
-import { GiftedChat, Send, Bubble } from "react-native-gifted-chat";
+import { View, Text, ToastAndroid } from "react-native";
+import { GiftedChat, Send, Bubble, IChatMessage } from "react-native-gifted-chat";
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 
+import { generate } from "../../utils/api";
+
 import AppStyles from "../../AppStyles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ActivityIndicator } from "react-native-paper";
 
 
 type ChatRoomPropsType = {
@@ -54,40 +58,82 @@ function ScrollToBottomComponent(props: any) {
 }
 
 export default function ChatRoom({ route, navigation }: ChatRoomPropsType) {
-  const [value, setValue] = useState<string>(route?.params?.initialValue)
-  const [messages, setMessages] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [responseLoading, setResponseLoading] = useState(false)
+  const [initialChat, setInitialChat] = useState<string>(route?.params?.initialValue)
+  const [token, setToken] = useState<string>()
+  const [previousMessages, setPreviousMessages] = useState<object[]>([])
+  const [messages, setMessages] = useState<IChatMessage[]>([])
+
+  // get user access-token
+  async function getUserToken() {
+    setLoading(true)
+    try {
+      const userToken = await AsyncStorage.getItem('@user')
+      if (userToken) {
+        const token = JSON.parse(userToken)
+        setToken(token)
+      } 
+      setLoading(false)
+    } catch (error) {
+      ToastAndroid.show(error as string, ToastAndroid.SHORT)
+      setLoading(false)
+    }
+  }
+
+  // get response from bot
+  async function getResponse(text:string) {
+    setResponseLoading(true)
+    if (token) {
+      const response = await generate(token, text)
+      if (Object.keys(response)[0] === 'result') {
+        const reply = {
+          user: {
+            _id: 2,
+            name: 'ChatGPT',
+          },
+          text: JSON.parse(response.result),
+        }
+        console.log(reply)
+        return reply
+      } else {
+        ToastAndroid.show('An error occured while generating response', ToastAndroid.SHORT)
+      }
+    }
+  }
 
   useEffect(() => {
-    setMessages([
-      {
-        id: 1,
-        text: 'Hello Developer',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React-Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-    ])
+    getUserToken()
+    return () => {}
   }, [])
 
   const onSend = useCallback((messages: []) => {
     setMessages(previousMessage => GiftedChat.append(previousMessage, messages))
+
+    // make request
+    const { text } = messages[0]
+    getResponse(text)
   }, [])
 
   return (
-    <GiftedChat
-      messages={messages}
-      onSend={messages => onSend(messages)}
-      user={{
-        _id: 1
-      }}
-      renderBubble={MessageBubble}
-      renderSend={RenderSend}
-      scrollToBottom
-      scrollToBottomComponent={ScrollToBottomComponent}
-    />
+    <>
+      {loading && (
+        <View style={{ flex: 1, backgroundColor: '#000', width: '100%', height: '100%', position: 'absolute', top: 0, right: 0, left: 0, bottom: 0, zIndex:99, opacity: 0.4 }} >
+          <ActivityIndicator color="white" />
+        </View>
+      )}
+      <GiftedChat
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1
+        }}
+        renderBubble={MessageBubble}
+        renderSend={RenderSend}
+        scrollToBottom
+        scrollToBottomComponent={ScrollToBottomComponent}
+      />
+    </>
   )
 };
 
--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -93,3 +93,33 @@ export async function verify(token:string) {
     }
     
 };
+
+export async function generate(token: string, message: string) {
+    const url = `${Address}/api/user/generate`
+
+    const payload = {
+        token: token,
+        message: message,
+    }
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(payload),
+        })
+        const responseJson = await response.json()
+        console.log("responseJson: ", responseJson)
+        if (response.status === 200) {
+            return { result: responseJson.result }
+        } else {
+            return { error: "An error occurred while generating the response." }
+        }
+    } catch (error) {
+        return { error: "An error occured. Try again later." }
+    }
+
+}
